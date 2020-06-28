3eb15b7f6365cf826dc4731023fdd14b5ba79dc1
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-06-27 23:20:18 -0400

    Updates

---
+++ precache-manifest.9833c7a275fa9b7add0b8c6cbae97887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "114d319b810f4cbb3936a6fe1aff49ea",
+    "url": "/roca-papel-tijeras/index.html"
+  },
+  {
+    "revision": "c849bd818a8b0ea3e390",
+    "url": "/roca-papel-tijeras/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e688dc6f302a5705728",
+    "url": "/roca-papel-tijeras/static/js/2.f575d758.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roca-papel-tijeras/static/js/2.f575d758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c849bd818a8b0ea3e390",
+    "url": "/roca-papel-tijeras/static/js/main.cd58f894.chunk.js"
+  },
+  {
+    "revision": "9efa5e9a88f394c9dfd6",
+    "url": "/roca-papel-tijeras/static/js/runtime-main.830d9d91.js"
+  }
+]);
